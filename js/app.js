f19f804bfa4bd86848bb05059d6fd276f008b85f
mnncolman <mncolman@herrera.unt.edu.ar>
2026-03-09 01:30:28 -0300

    update - cambios visuales de css y nueva restriccion de filtro para evitar mostrar todos los gabientes junto con todos las divisiones

--- js/app.js
+++ js/app.js
@@ -5,8 +5,6 @@ var calendar = null;
 
 document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
-    var loadingEl = document.getElementById('loading');
-    var selector = document.getElementById('selectorEspacio');
 
     // 2. Función asíncrona para traer todo de UN SOLO GOLPE
     async function cargarDatos() {
@@ -17,12 +15,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
             todosLosEventos = datos.eventos;
 
-            // Llenamos la UI al instante
             llenarBuscadorDocentesNormalizado(datos.docentes);
             llenarSelectorGabinetesNormalizado(datos.gabinetes);
 
-            // Cargamos el calendario
-            calendar.addEventSource(datos.eventos);
+            aplicarFiltros();
 
             // Matamos el spinner
             const loadingEl = document.getElementById('contenedor-carga');
@@ -54,11 +50,6 @@ document.addEventListener('DOMContentLoaded', function () {
         },
 
         views: {
-            timeGridSemanaLaboral: {
-                type: 'timeGrid',
-                duration: { days: 6 }, // 5 dias
-                buttonText: 'Lun-Vie'
-            },
             timeGridTresDias: {
                 type: 'timeGrid',
                 duration: { days: 3 },  // 3 dias
@@ -71,16 +62,12 @@ document.addEventListener('DOMContentLoaded', function () {
             timeGridWeek: {
                 buttonText: 'semana'
             },
-            dayGridMonth: {
-                buttonText: 'mes'
-            },
             listWeek: {
                 buttonText: 'agenda' // Por si usas la vista de lista en celulares
             }
         },
 
-        firstDay: 1,          // 0 = domingo, 1 = lunes
-        weekends: false,      // oculta sábado y domingo
+        firstDay: 1,
 
         displayEventTime: false,
         allDaySlot: false,
@@ -117,7 +104,7 @@ document.addEventListener('DOMContentLoaded', function () {
               <p>Horario: ${horaInicio} a ${horaFin}</p>
               <p>Gabinete: ${props.gabinete || ''}</p>
             `,
-                confirmButtonText: 'Entendido'
+                confirmButtonText: 'Cerrar'
             });
         },
 
@@ -127,16 +114,19 @@ document.addEventListener('DOMContentLoaded', function () {
             hour12: false,        // false = 13:00 | true = 1:00 pm
             meridiem: false       // Ocultar am/pm si usas 24hs
         },
-        slotDuration: '00:15:00', 
+        slotDuration: '00:15:00',
         slotMinTime: '07:00:00',  // El calendario arranca visualmente a las 7 AM
         slotMaxTime: '23:00:00',  // Termina a las 11 PM
 
         events: []
     });
 
-    calendar.render();
-
     cargarDatos();
+
+    aplicarFiltros();
+    
+
+    calendar.render();
 
     document.getElementById('selectorEspacio')
         .addEventListener('change', aplicarFiltros);
@@ -194,96 +184,104 @@ function llenarSelectorGabinetesNormalizado(listaGabinetes) {
 
 // --- LA FUNCIÓN DE FILTRADO ---
 function aplicarFiltros() {
-    // Capturamos lo que el usuario escribió/eligió (pasado a minúsculas para que no falle)
-
-    let textoDivision = document.getElementById('filtro-division').value.toLowerCase();
-    let textogabinete = document.getElementById('selectorEspacio').value.toLowerCase();
-
-    let textoDocente = document.getElementById('buscar-docente').value.toLowerCase().trim();
-    let panelColegas = document.getElementById('panel-colegas');
     let divListaColegas = document.getElementById('lista-colegas');
+    /*   
+   // Solo activamos este cálculo pesado si escribieron al menos 3 letras 
+   // (para no buscar coincidencia con la letra "a" y colgar el navegador)
+   if (textoDocente.length >= 3) {
 
-    // Solo activamos este cálculo pesado si escribieron al menos 3 letras 
-    // (para no buscar coincidencia con la letra "a" y colgar el navegador)
-    if (textoDocente.length >= 3) {
+       // PASO 1: Encontrar los gabinetes que usa ESTE docente
+       let gabinetesDelDocente = new Set();
 
-        // PASO 1: Encontrar los gabinetes que usa ESTE docente
-        let gabinetesDelDocente = new Set();
+       todosLosEventos.forEach(evento => {
+           let docenteEv = (evento.extendedProps.responsable || '').toLowerCase();
+           let ubicacionEv = evento.extendedProps.gabinete;
 
-        todosLosEventos.forEach(evento => {
-            let docenteEv = (evento.extendedProps.responsable || '').toLowerCase();
-            let ubicacionEv = evento.extendedProps.gabinete;
+           if (docenteEv.includes(textoDocente) && ubicacionEv) {
+               gabinetesDelDocente.add(ubicacionEv);
+           }
+       });
 
-            if (docenteEv.includes(textoDocente) && ubicacionEv) {
-                gabinetesDelDocente.add(ubicacionEv);
-            }
-        });
+       // PASO 2: Si encontramos gabinetes, buscamos a los colegas
+       if (gabinetesDelDocente.size > 0) {
+           let colegasPorGabinete = {}; // Guardaremos { "GAB 1": Set(["Profe A", "Profe B"]) }
 
-        // PASO 2: Si encontramos gabinetes, buscamos a los colegas
-        if (gabinetesDelDocente.size > 0) {
-            let colegasPorGabinete = {}; // Guardaremos { "GAB 1": Set(["Profe A", "Profe B"]) }
+           // Inicializamos los sets vacíos para cada gabinete
+           gabinetesDelDocente.forEach(gab => colegasPorGabinete[gab] = new Set());
 
-            // Inicializamos los sets vacíos para cada gabinete
-            gabinetesDelDocente.forEach(gab => colegasPorGabinete[gab] = new Set());
+           // Recorremos la base de datos OTRA VEZ para encontrar a los invasores... digo, colegas
+           todosLosEventos.forEach(evento => {
+               let gab = evento.extendedProps.gabinete;
+               let doc = evento.extendedProps.responsable || 'Sin asignar';
 
-            // Recorremos la base de datos OTRA VEZ para encontrar a los invasores... digo, colegas
-            todosLosEventos.forEach(evento => {
-                let gab = evento.extendedProps.gabinete;
-                let doc = evento.extendedProps.responsable || 'Sin asignar';
+               // Si el evento ocurre en uno de "nuestros" gabinetes, y el profe NO es el que estamos buscando
+               if (gabinetesDelDocente.has(gab) && !doc.toLowerCase().includes(textoDocente)) {
+                   colegasPorGabinete[gab].add(doc); // Lo agregamos al set (evita duplicados automáticamente)
+               }
+           });
 
-                // Si el evento ocurre en uno de "nuestros" gabinetes, y el profe NO es el que estamos buscando
-                if (gabinetesDelDocente.has(gab) && !doc.toLowerCase().includes(textoDocente)) {
-                    colegasPorGabinete[gab].add(doc); // Lo agregamos al set (evita duplicados automáticamente)
-                }
-            });
+           // PASO 3: Armar el texto visual para el director
+           let htmlResultado = '';
+           for (let gabinete in colegasPorGabinete) {
+               let arrayColegas = Array.from(colegasPorGabinete[gabinete]).sort();
 
-            // PASO 3: Armar el texto visual para el director
-            let htmlResultado = '';
-            for (let gabinete in colegasPorGabinete) {
-                let arrayColegas = Array.from(colegasPorGabinete[gabinete]).sort();
+               if (arrayColegas.length > 0) {
+                   // Ejemplo visual: • Gabinete 4: Perillo, Fernandez M.
+                   htmlResultado += `<div><b>• ${gabinete}:</b> ${arrayColegas.join(', ')}</div>`;
+               } else {
+                   htmlResultado += `<div><b>• ${gabinete}:</b> (Nadie más usa este espacio)</div>`;
+               }
+           }
 
-                if (arrayColegas.length > 0) {
-                    // Ejemplo visual: • Gabinete 4: Perillo, Fernandez M.
-                    htmlResultado += `<div><b>• ${gabinete}:</b> ${arrayColegas.join(', ')}</div>`;
-                } else {
-                    htmlResultado += `<div><b>• ${gabinete}:</b> (Nadie más usa este espacio)</div>`;
-                }
-            }
+           // Mostramos el panel y le pegamos el texto
+           divListaColegas.innerHTML = htmlResultado;
+           panelColegas.style.display = 'block';
 
-            // Mostramos el panel y le pegamos el texto
-            divListaColegas.innerHTML = htmlResultado;
-            panelColegas.style.display = 'block';
+       } else {
+           // Si escribió un nombre que no tiene clases asignadas
+           panelColegas.style.display = 'none';
+       }
 
-        } else {
-            // Si escribió un nombre que no tiene clases asignadas
-            panelColegas.style.display = 'none';
-        }
+   } else {
+       // Si borró el buscador o hay menos de 3 letras, ocultamos el panel
+       panelColegas.style.display = 'none';
+   }
 
-    } else {
-        // Si borró el buscador o hay menos de 3 letras, ocultamos el panel
-        panelColegas.style.display = 'none';
+*/
+
+
+    let selectorDivision = document.getElementById('filtro-division');
+    let selectorGabinete = document.getElementById('selectorEspacio');
+    let textoDocente = document.getElementById('buscar-docente').value.toLowerCase().trim();
+
+    // Si ambos están vacíos y tampoco hay docente, forzamos el primer valor real de división
+    if (selectorDivision.value === '' && selectorGabinete.value === '' && textoDocente === '') {
+        // Buscamos la primera opción que no sea la vacía
+        let primeraOpcion = Array.from(selectorDivision.options).find(op => op.value !== '');
+        if (primeraOpcion) selectorDivision.value = primeraOpcion.value;
     }
+
+    let textoDivision = selectorDivision.value.toLowerCase();
+    let textogabinete = selectorGabinete.value.toLowerCase();
+
 
 
     let eventosFiltrados = todosLosEventos.filter(evento => {
-
         let docenteDelEvento = (evento.extendedProps.responsable || '').toLowerCase();
         let divisionDelEvento = (evento.extendedProps.division || '').toLowerCase();
         let gabineteDelEvento = (evento.extendedProps.gabinete || '').toLowerCase();
 
         let coincideDocente = docenteDelEvento.includes(textoDocente);
-        let coincideDivision = textoDivision === "" || divisionDelEvento === textoDivision;
-        let coincidegabinete = textogabinete === "" || gabineteDelEvento === textogabinete;
+        let coincideDivision = textoDivision === '' || divisionDelEvento === textoDivision;
+        let coincidegabinete = textogabinete === '' || gabineteDelEvento === textogabinete;
 
-
-        return (coincideDocente && coincideDivision && coincidegabinete);
+        return coincideDocente && coincideDivision && coincidegabinete;
     });
-
 
     calendar.removeAllEventSources();
     calendar.addEventSource(eventosFiltrados);
-}
 
+}
 
 
 function procesarDatosYCrearOpciones(listaDeEspacios) {
@@ -333,7 +331,7 @@ document.getElementById('btn-descargar').addEventListener('click', async functio
         top: -9999px;
         left: -9999px;
         width: 1400px;
-        height: 500px;
+        height: 700px;
         z-index: -1;
         background: white;
     `;
@@ -342,15 +340,15 @@ document.getElementById('btn-descargar').addEventListener('click', async functio
     const pdfW = doc.internal.pageSize.getWidth();
     const pdfH = doc.internal.pageSize.getHeight();
     const turnos = [
-        { nombre: 'Mañana', slotMin: '07:00:00', slotMax: '14:00:00' },
-        { nombre: 'Tarde', slotMin: '14:00:00', slotMax: '22:00:00' }
+        { nombre: 'Mañana', slotMin: '07:00:00', slotMax: '14:05:00' },
+        { nombre: 'Tarde', slotMin: '14:00:00', slotMax: '20:05:00' }
     ];
     const capturas = [];
 
     for (const turno of turnos) {
         // 2. Creamos un calendario temporal con dimensiones fijas
         const calTemp = new FullCalendar.Calendar(tempDiv, {
-            initialView: 'timeGridSemanaLaboral',
+            initialView: 'timeGridWeek',
             initialDate: fechaInicio,
             locale: 'es',
             weekends: false,
@@ -359,16 +357,24 @@ document.getElementById('btn-descargar').addEventListener('click', async functio
             allDaySlot: false,
             slotMinTime: turno.slotMin,
             slotMaxTime: turno.slotMax,
+
+            slotLabelFormat: {
+                hour: '2-digit',      // 'numeric' para 1, 2... | '2-digit' para 01, 02...
+                minute: '2-digit',    // Mostrar minutos: 00
+                hour12: false,        // false = 13:00 | true = 1:00 pm
+                meridiem: false       // Ocultar am/pm si usas 24hs
+            },
             slotDuration: '00:15:00',
-            height: 500,
-            contentHeight: 500,
+            height: 700,
+            contentHeight: 700,
             views: {
-                timeGridSemanaLaboral: {
+                timeGridWeek: {
                     type: 'timeGrid',
-                    duration: { days: 5 }
                 }
             },
             displayEventTime: false,
+            weekends: false,
+            firstDay: 1,
 
             events: (() => {
                 // Leemos los filtros activos igual que en aplicarFiltros()
@@ -413,10 +419,10 @@ document.getElementById('btn-descargar').addEventListener('click', async functio
 
         // 3. Capturamos el calendario temporal
         const canvas = await html2canvas(tempDiv, {
-            scale: 2,
+            scale: 1.6,
             useCORS: true,
             width: 1400,
-            height: 500,
+            height: 700,
             scrollX: 0,
             scrollY: 0
         });
@@ -431,22 +437,39 @@ document.getElementById('btn-descargar').addEventListener('click', async functio
     // 5. Limpiamos el contenedor temporal del DOM
     document.body.removeChild(tempDiv);
 
-    // 6. Armamos el PDF
-    const headerAltura = 6;
-    const margen = 4;
-    const espacioPorTurno = (pdfH - (headerAltura + margen) * 2) / 2;
-
+    // 6. Armamos el PDF en 2 páginas
     capturas.forEach((item, i) => {
-        const imgData = item.canvas.toDataURL('image/png');
-        const offsetY = i * (espacioPorTurno + headerAltura + margen);
-        doc.setFontSize(7);
-        doc.text(`Semana  |  ${etiquetaVista}  |  Horario ${item.nombre}`, 4, offsetY + headerAltura);
-        doc.addImage(imgData, 'PNG', 0, offsetY + headerAltura + 1, pdfW, espacioPorTurno);
-    });
 
+        const imgData = item.canvas.toDataURL('image/png', 0.95);
+        const pdfW = doc.internal.pageSize.getWidth();
+        const pdfH = doc.internal.pageSize.getHeight();
+
+        if (i > 0) doc.addPage();
+
+        doc.setFontSize(13);
+        doc.text(`Semana  |  ${etiquetaVista}  |  Horario ${item.nombre}`, 8, 8);
+
+        const margen = 5; // 👈 cambiá este valor a gusto (en mm)
+        doc.addImage(
+            imgData, 'JPEG',
+            margen,           // margen izquierdo
+            20,               // margen superior (respeta el encabezado)
+            pdfW - margen * 3,  // ancho - margen derecho e izquierdo
+            pdfH - 40 * 2,  // alto - encabezado - margen inferior
+            '',                    // alias (vacío)
+            'MEDIUM'
+        );
+    });
     doc.save(`horario-${etiquetaVista}.pdf`);
 });
 
 document.getElementById('selector-vista').addEventListener('change', function () {
-    calendar.changeView(this.value);
+    if (this.value === 'timeGridSemanaLaboral') {
+        calendar.setOption('weekends', false);
+        calendar.changeView('timeGridWeek');
+
+    } else {
+        calendar.setOption('weekends', true);
+        calendar.changeView(this.value);
+    }
 });
